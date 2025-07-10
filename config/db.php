e61d5181fa3cf893b3b4b1315cdb2f91ff48d8b1
Abadir Hassan <34312697+Boodhari@users.noreply.github.com>
2025-07-10 23:46:06 +0300

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,6 @@
+<?php
+$conn = new mysqli("sql8.freesqldatabase.com", "sql8789367 ", "FAVAsfUJqS", "sql8789367");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
