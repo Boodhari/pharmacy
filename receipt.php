1b069a376a8280d81b2e6a3c000c51357629f290
Abadir Hassan <34312697+Boodhari@users.noreply.github.com>
2025-07-13 14:59:58 +0300

    no print function to print button

--- receipt.php
+++ receipt.php
@@ -100,7 +100,7 @@ while ($row = $result->fetch_assoc()) {
     </tfoot>
   </table>
 
-  <div class="text-center mt-4">
+  <div class="text-center mt-4 no-print">
     <button onclick="window.print()" class="btn btn-primary">🖨️ Print</button>
   </div>
 </div>
