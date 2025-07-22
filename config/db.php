5dd74f4daa07bc41665f7c72a972505fb1d60bb7
Abadir Hassan <34312697+Boodhari@users.noreply.github.com>
2025-07-22 18:58:34 +0300

    config db

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "Dentals");
+$conn = new mysqli("sql8.freesqldatabase.com", "sql8791337", "WGAG6qxESm", "sql8791337");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
