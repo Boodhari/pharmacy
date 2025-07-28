6ec0f27144161f09271b77f9a71567d0ec7a952d
Abadir Hassan <34312697+Boodhari@users.noreply.github.com>
2025-07-28 10:43:55 +0300

    modifiying header 1 and admin dashboard

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -16,6 +16,32 @@ $result = $conn->query("SELECT * FROM clinics ORDER BY id DESC");
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="container py-5">
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+         <div class="container-fluid">
+             <a class="navbar-brand" href="admin_dashboard.php">Admin Dashboard</a>
+             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                 aria-label="Toggle navigation">
+                 <span class="navbar-toggler-icon"></span>
+             </button>
+             <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                     <li class="nav-item">
+                         <a class="nav-link active" aria-current="page" href="#">Home</a>
+                     </li>
+                     <li class="nav-item">
+                         <a class="nav-link" href="register_user.php">Register Users</a>
+                     </li>
+                     <li class="nav-item">
+                         <a class="nav-link" href="create_clinic.php">New Clinics</a>
+                     </li>
+                     <li class="nav-item">
+                         <a class="nav-link" href="Logout.php">Logout</a>
+                     </li>
+                 </ul>
+             </div>
+         </div>
+     </nav>
   <h2 class="mb-4">🏥 Super Admin Dashboard</h2>
   <a href="create_clinic.php" class="btn btn-success mb-3">+ Create New Clinic</a>
  
